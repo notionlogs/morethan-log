ed453bebffbe102701e7478c0051bb86780dc166
notionlogs <152959091+notionlogs@users.noreply.github.com>
2023-12-05 17:17:40 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,26 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Notion Logs",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "lifelogger",
+    bio: "I log everything in my life.",
+    email: "notionlogs@gmail.com",
+    linkedin: "",
+    github: "notionlogs",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `Life Logs`,
+      href: "https://lifelogging.com/@notionlogs",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "Life Logs",
+    description: "Life Logs!",
+    theme: "auto", // ['light', 'dark', 'auto']
   },
 
   // CONFIG configration (required)
@@ -47,7 +48,7 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: true,
+    enable: false,
     config: {
       repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
       "issue-term": "og:title",
